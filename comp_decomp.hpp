158c6ff87dd32664a61b1daf0f3c56429bea0246
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2019-02-10 15:48:02 +0900

    simplify comp_decomp.hpp

--- comp_decomp.hpp
+++ comp_decomp.hpp
@@ -1,29 +1,6 @@
 #ifndef COMP_DECOMP_HPP_
 #define COMP_DECOMP_HPP_
 
-#undef min
-
-#include <cstdlib>
-#include <cstdint>
-#include <string>
-#include <algorithm>
-
-#ifndef COMP_DECOMP_BUFFSIZE
-    #define COMP_DECOMP_BUFFSIZE (8 * 1024)
-#endif
-#ifndef COMP_DECOM_MIN_TEST
-    #define COMP_DECOM_MIN_TEST 0
-#endif
-#ifndef COMP_DECOM_MAX_TEST
-    #define COMP_DECOM_MAX_TEST 100
-#endif
-#ifndef COMP_DECOM_TEST_STEP
-    #define COMP_DECOM_TEST_STEP 10
-#endif
-#ifndef COMP_DECOM_COMPRESSION_RATE
-    #define COMP_DECOM_COMPRESSION_RATE 9
-#endif
-
 #ifdef HAVE_ZLIB
     #include "comp_decomp_zlib.hpp"
 #endif  // def HAVE_ZLIB
